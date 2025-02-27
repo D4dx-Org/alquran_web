908d9f3265180c1bdc1243a4836f79f9f9fdfc7b
junaidh haneefa <junaidh.2307@gmail.com>
2025-02-27 12:34:32 +0530

    🗃️ Update database initialization and asset management

--- lib/services/dbservice.dart
+++ lib/services/dbservice.dart
@@ -2,100 +2,81 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'dart:typed_data';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:archive/archive.dart';
-import 'package:archive/archive_io.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 
 class DBService {
   static final DBService dbProvider = DBService();
   Database? db;
-  static const NEW_DB_VERSION = 3;
+  static const NEW_DB_VERSION = 4;
 
-  Future<Database> initDB(
-      {String path = "alquran3.db",
-      bool absolutePath = false,
-      bool verbose = true,
-      String fromAsset = "assets/alquran.zip",
-      bool debug = false}) async {
+  Future<Database> initDB({
+    String path = "alquran4.db",
+    bool absolutePath = false,
+    bool verbose = true,
+    String fromAsset = "assets/alquran4.db", // Changed from zip to direct db file
+    bool debug = false
+  }) async {
     /// The [path] is where the database file will be stored. It is by
     /// default relative to the documents directory unless [absolutePath]
     /// is true.
-    /// [queries] is a list of queries to run at initialization
-    /// and [debug] set Sqflite debug mode on
     if (debug) Sqflite.setDebugModeOn(true);
     String dbpath = path;
-    String zippath = 'alquran.zip';
-    String oldDbPath = "alquran2.db";
+    String oldDbPath = "alquran3.db";
     if (!absolutePath) {
       Directory documentsDirectory = await getApplicationDocumentsDirectory();
-      oldDbPath = "${documentsDirectory.path}/alquran2.db";
+      oldDbPath = "${documentsDirectory.path}/alquran3.db";
       dbpath = "${documentsDirectory.path}/$path";
-      zippath = "${documentsDirectory.path}/$zippath";
     }
 
     /// delete if version is old
-    var thafdb = await openDatabase(dbpath);
-    //if database does not exist yet it will return version 0
     if (await databaseExists(oldDbPath)) await deleteDatabase(oldDbPath);
-    if (await thafdb.getVersion() < NEW_DB_VERSION) {
-      thafdb.close();
-      //delete the old database so you can copy the new one
-      if (verbose) {}
-      if (await databaseExists(dbpath)) await deleteDatabase(dbpath);
-    }
-
-    if (verbose) {}
-    // copy the database from an asset if necessary
-    if (fromAsset != "") {
-      File file = File(dbpath);
-      File zipFile = File(zippath);
-      if (!file.existsSync()) {
-        if (verbose) {}
-        List<int> bytes;
-        try {
-          // read zip file
-          ByteData data = await rootBundle.load(fromAsset);
-          bytes =
-              data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-        } catch (e) {
-          throw ("Unable to read database from asset: $e");
-        }
-        try {
-          // create the directories path if necessary
-          if (!file.parent.existsSync()) {
-            file.parent.createSync(recursive: true);
-          }
-          // write zip file
-          await zipFile.writeAsBytes(bytes);
-        } catch (e) {
-          throw ("Unable to write database zip from asset: $e");
-        }
-        // Read the Zip file from disk.
-        bytes = File(zipFile.path).readAsBytesSync();
-
-        // Decode the Zip file
-        Archive archive = ZipDecoder().decodeBytes(bytes);
-
-        // Extract the contents of the Zip archive to disk.
-        for (ArchiveFile dbfile in archive) {
-          //String filename = dbfile.name;
-          if (dbfile.isFile) {
-            List<int> data = dbfile.content;
-            File(dbpath)
-              ..createSync(recursive: true)
-              ..writeAsBytesSync(data);
-          }
-        }
-        await zipFile.delete(recursive: false);
+    
+    // Check if database already exists and its version
+    if (await databaseExists(dbpath)) {
+      var thafdb = await openDatabase(dbpath);
+      if (await thafdb.getVersion() < NEW_DB_VERSION) {
+        thafdb.close();
+        // Delete outdated database
+        if (verbose) print("Deleting old database version");
+        await deleteDatabase(dbpath);
+      } else {
+        thafdb.close();
       }
     }
-    var myDatabase = await openDatabase(dbpath, version: 3, readOnly: false,
-        onCreate: (Database db, int version) async {
-      db.setVersion(NEW_DB_VERSION);
-      this.db = db;
-    });
-    if (verbose) {}
+
+    // Copy database from assets if it doesn't exist or was deleted due to version
+    if (!await databaseExists(dbpath)) {
+      if (verbose) print("Copying database from assets");
+      try {
+        // Read database file from assets
+        ByteData data = await rootBundle.load(fromAsset);
+        List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+        
+        // Create directories if needed
+        File file = File(dbpath);
+        if (!file.parent.existsSync()) {
+          file.parent.createSync(recursive: true);
+        }
+        
+        // Write database file
+        await file.writeAsBytes(bytes, flush: true);
+      } catch (e) {
+        throw ("Unable to copy database from asset: $e");
+      }
+    }
+
+    // Open the database
+    var myDatabase = await openDatabase(
+      dbpath, 
+      version: NEW_DB_VERSION, 
+      readOnly: false,
+      onCreate: (Database db, int version) async {
+        db.setVersion(NEW_DB_VERSION);
+        this.db = db;
+      }
+    );
+    
+    if (verbose) print("Database opened successfully");
     return myDatabase;
   }
 
@@ -103,14 +84,17 @@ class DBService {
     try {
       // Get a location using getDatabasesPath
       var databasesPath = await getDatabasesPath();
-      String path = join(databasesPath, 'alquran3.db');
+      String path = join(databasesPath, 'alquran4.db');
       final exist = await databaseExists(path);
       if (exist) {
         var db = await openDatabase(
           path,
-          version: 3,
+          version: NEW_DB_VERSION,
         );
         this.db = db;
+      } else {
+        // If the database doesn't exist in the system path, initialize it
+        return await initDB();
       }
       // open the database
       return true;
@@ -125,7 +109,7 @@ class DBService {
   }
 
   close() async {
-    var db = database;
+    var db = await database;
     var result = db.close();
     return result;
   }
