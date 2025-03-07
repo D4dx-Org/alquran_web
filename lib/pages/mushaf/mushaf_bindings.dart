0ea984cbd6a09b69fbd9820052450dccb838659d
junaidh haneefa <junaidh.2307@gmail.com>
2025-03-07 10:10:16 +0530

    🔧 Add IndexController registration in Mushaf and TransListing bindings

--- lib/pages/mushaf/mushaf_bindings.dart
+++ lib/pages/mushaf/mushaf_bindings.dart
@@ -1,9 +1,13 @@
+import 'package:alquran_malayalam/pages/index/index_controller.dart';
 import 'package:alquran_malayalam/pages/mushaf/mushaf_controller.dart';
 import 'package:get/instance_manager.dart';
 
 class MushafBindings extends Bindings {
   @override
   void dependencies() {
+    if (!Get.isRegistered<IndexController>()) {
+      Get.put<IndexController>(IndexController(), permanent: true);
+    }
     Get.lazyPut<MushafController>(() => MushafController());
   }
 }
--- lib/pages/tranlisting/tranlisting_binding.dart
+++ lib/pages/tranlisting/tranlisting_binding.dart
@@ -1,9 +1,13 @@
 import 'package:get/instance_manager.dart';
 import 'package:alquran_malayalam/pages/tranlisting/tranlisting_controller.dart';
+import 'package:alquran_malayalam/pages/index/index_controller.dart';
 
 class TranListingBinding extends Bindings {
   @override
   void dependencies() {
+    if (!Get.isRegistered<IndexController>()) {
+      Get.put<IndexController>(IndexController(), permanent: true);
+    }
     Get.lazyPut<TranListingController>(() => TranListingController());
   }
 }
